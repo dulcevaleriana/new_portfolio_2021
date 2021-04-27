4b46301734e63602bdacad589deb8fe3e2c9f762
dulcevaleria <dulceguzmantaveras@gmail.com>
2021-04-27 12:18:35 -0400

    primera animacion caminata de frente hecha !

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-//import animacion_1_prueba from './image/animacion-frame_Mesa de trabajo1.png';
 import './css/style.scss';
 
 function App() {
@@ -8,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* Prueba animacion caminando de frente */}
+        <div className="class-animacion-0"/>
         <div className="class-animacion-1"/>
       </header>
     </div>
