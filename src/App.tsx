9a41804f55bc08469749c657666da08ad688b05a
dulcevaleria <dulceguzmantaveras@gmail.com>
2021-05-03 09:34:50 -0400

    avance menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Menu from './component/nemu';
+
 import './App.css';
 import './css/style.scss';
 
-//import Animate from './component/animate';
-
+import Animate from './component/animate';
+const menuList = [
+  {
+    link:'/',
+    name:'Portfolio',
+    page:<Animate/>
+  },
+  {
+    link:'/aboutUs',
+    name:'About Us',
+    page:'lala'
+  },
+  {
+    link:'/contact',
+    name:'Contact',
+    page:'lolo'
+  }
+];
 function App() {
   return (
-    <div className="App">
-      {/* <Animate/> */}
-    </div>
+    <Router>
+      <>
+      <Menu menuList={menuList}/>
+        <Switch>
+          {menuList.map((list:any,i:any) =>
+            <Route key={i} exact path={list.link}>
+              {list.page}
+            </Route>
+          )}
+        </Switch>
+      </>
+    </Router>
   );
 }
 
