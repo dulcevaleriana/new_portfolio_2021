3e4e4095a264c02008192efe1fe49c956331a750
dulcevaleria <dulceguzmantaveras@gmail.com>
2021-05-04 11:16:11 -0400

    cuarto paso

--- src/view/portfolio.tsx
+++ src/view/portfolio.tsx
@@ -1,10 +1,22 @@
 import React, { useState } from 'react';
+
 import MESA from '../image/mesa.svg';
+import IMG1 from '../image/Web1920–6.png';
+import IMG2 from '../image/Web1920–2.png';
+import IMG3 from '../image/Web1920–1.png';
+import IMG4 from '../image/Image6.png';
+import IMG5 from '../image/Image5.png';
+import IMG6 from '../image/Image4.png';
+import IMG7 from '../image/Image3.png';
+import IMG8 from '../image/Image2.png';
+import IMG9 from '../image/Image1.png';
 
 const Portfolio = (props:any) => {
     const [viewPage1, setPage1] = useState(true);
     const [viewPage2, setPage2] = useState(false);
     const [viewPage3, setPage3] = useState(false);
+
+    let ClassElementActive = 'active';
 
     const selectThis1 = () =>{
         setPage1(true);
@@ -31,33 +43,39 @@ const Portfolio = (props:any) => {
                 <div>
                     <p>Portfolio</p>
                     {viewPage1 &&
-                    <h2>1</h2>
+                    <h2>My Portfolio Evolution</h2>
                     }
                     {viewPage2 &&
-                    <h2>2</h2>
+                    <h2>Green Card</h2>
                     }
                     {viewPage3 &&
-                    <h2>3</h2>
+                    <h2>Unicef</h2>
                     }
                 </div>
                 <div>
-                    <div onClick={() => selectThis1()}/>
-                    <div onClick={() => selectThis2()}/>
-                    <div onClick={() => selectThis3()}/>
+                    <div className={`${viewPage1 ? ClassElementActive : ""}`} onClick={() => selectThis1()}/>
+                    <div className={`${viewPage2 ? ClassElementActive : ""}`} onClick={() => selectThis2()}/>
+                    <div className={`${viewPage3 ? ClassElementActive : ""}`} onClick={() => selectThis3()}/>
                 </div>
                 {viewPage1 &&
                 <div>
-1
+                    <img src={IMG9} alt="IMG1"/>
+                    <img src={IMG8} alt="IMG1"/>
+                    <img src={IMG7} alt="IMG1"/>
                 </div>
                 }
                 {viewPage2 &&
                 <div>
-2
+                    <img src={IMG6} alt="IMG1"/>
+                    <img src={IMG5} alt="IMG1"/>
+                    <img src={IMG4} alt="IMG1"/>
                 </div>
                 }
                 {viewPage3 &&
                 <div>
-3
+                    <img src={IMG1} alt="IMG1"/>
+                    <img src={IMG2} alt="IMG1"/>
+                    <img src={IMG3} alt="IMG1"/>
                 </div>
                 }
             </div>
