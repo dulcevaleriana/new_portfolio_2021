3ce16ed5a1caedbe7003a9b53bae4a894bbcd5bb
dulcevaleria <dulceguzmantaveras@gmail.com>
2021-05-06 08:34:05 -0400

    primeros pasos aplicacion de campos

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import Portfolio from './view/portfolio';
 import AboutUs from './view/aboutUs';
 import Contact from './view/contact';
 
-// import Animate from './component/animate';
+import Animate from './component/animate';
 const menuList = [
   {
     link:'/',
@@ -36,12 +36,13 @@ const menuList = [
     link:'/contact',
     name:'Contact',
     page:<Contact/>
-  },
-  // {
-  //   link:'/animatePrueba',
-  //   name:'animatePrueba',
-  //   page:<Animate/>
-  // }
+  }
+  ,
+  {
+    link:'/animatePrueba',
+    name:'animatePrueba',
+    page:<Animate/>
+  }
 ];
 function App() {
   return (
--- src/view/aboutUs.tsx
+++ src/view/aboutUs.tsx
@@ -1,7 +1,27 @@
+import MESA from '../image/mesa.svg';
+import SILLA from '../image/silla.svg';
+
 const aboutUs = (props:any) => {
     return(
         <div className="class-aboutUs">
-            aboutUs
+            <div></div>
+            <div>
+                <div>
+                    <h1>Hello</h1>
+                    <h1>I'm <strong>Dulce Valeriana</strong></h1>
+                    <h2>Front End / Web Developer</h2>
+                </div>
+                <div>
+                    <h1>Hello</h1>
+                    <h1>I'm <strong>Dulce Valeriana</strong></h1>
+                    <h2>Front End / Web Developer</h2>
+                </div>
+            </div>
+            <div>
+                <div className="class-animacion-0"/>
+                <img src={MESA} alt="MESA"/>
+                <img src={SILLA} alt="MESA"/>
+            </div>
         </div>
     )
 }
