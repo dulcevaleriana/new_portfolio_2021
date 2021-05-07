037dc83678bf96c129c2acfa22705a657cb7c6b9
dulcevaleria <dulceguzmantaveras@gmail.com>
2021-05-07 11:15:34 -0400

    done contact form

--- src/view/contact.tsx
+++ src/view/contact.tsx
@@ -1,7 +1,29 @@
+import {Link} from "react-router-dom";
+import MESA from '../image/mesa.svg';
+
 const contact = (props:any) => {
     return(
         <div className="class-contact">
-            contact
+            <div></div>
+            <div>
+                <p>Contact</p>
+                <h2>Connect whit me !</h2>
+                <input type="text" id="fname" name="fname" placeholder="name"/>
+                <input type="text" id="fname" name="fname" placeholder="name"/>
+                <input type="text" id="fname" name="fname" placeholder="name"/>
+                <textarea id="w3review" name="w3review">
+                    Write Here
+                </textarea>
+                <Link to="/contact">
+                    <button>
+                        Contact me
+                    </button>
+                </Link>
+            </div>
+            <div>
+                <div className="class-aperson-sit"/>
+                <img src={MESA} alt="MESA"/>
+            </div>
         </div>
     )
 }
